90ae0db7b30b447060cd2a18bdc8f09e6af60475
Vinícius Moreira <vsmoreira@gmail.com>
2015-06-05 05:28:14 +0000

    Aulas 31, 32 e 33. Criaçao e ediçao de receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,41 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)
+    
+    if @recipe.save
+      flash[:success] = "Humm, sua receita foi criada com sucesso."
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    
+    if @recipe.update(recipe_params)
+      flash[:success] = "Receita alterada com sucesso!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+  
+  private
+    
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+  
 end
