fc4b963b59b9cc165b435d067c7c00450f31cf41
Vinícius Moreira <vsmoreira@gmail.com>
2015-06-06 19:10:18 +0000

    Aulas 35, 36 e 37 - Image upload

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,14 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true, length: { within: 5..100 }
   validates :summary, presence: true, length: { within: 10..150 }
   validates :description, presence: true, length: { within: 20..500 }
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+    def picture_size 
+      if picture.size > 5.megabytes
+        errors.add(:picture, "Should be last than 5MB")
+      end
+    end
 end
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514033650) do
+ActiveRecord::Schema.define(version: 20150606145226) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150514033650) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
