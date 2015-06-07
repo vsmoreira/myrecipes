970b4b28b2dd5357c97dd9d4701dd38a71e2ae97
Vinícius Moreira <vsmoreira@gmail.com>
2015-06-07 05:43:19 +0000

    Aulas 38, 39 e 40 - Joinha nas receitas

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,20 @@
 class Recipe < ActiveRecord::Base
   belongs_to :chef
+  has_many :likes
   validates :chef_id, presence: true
   validates :name, presence: true, length: { within: 5..100 }
   validates :summary, presence: true, length: { within: 10..150 }
   validates :description, presence: true, length: { within: 20..500 }
   mount_uploader :picture, PictureUploader
   validate :picture_size
+  
+  def thumbs_up_total
+    self.likes.where(like: true).size
+  end
+  
+  def thumbs_down_total
+    self.likes.where(like: false).size
+  end
   
   private
     def picture_size 
