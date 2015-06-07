6b869ee6bf2e3ae8ae6ab38f0b563f420c92871d
Vinícius Moreira <vsmoreira@gmail.com>
2015-06-07 15:46:06 +0000

    Aula 41 - Paginaçao nas receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
   
   def index
-    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
+    #Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
+    @recipes = Recipe.paginate(page: params[:page], per_page: 4)
   end
   
   def show
